0a5f1df42e409562059dffdd79960c3db1ed37aa
Nils Mooldijk <nils@Nils-MacBook.local>
2023-02-13 15:01:11 +0100

    initial commit

---
+++ src/main/java/nl/bioinf/cuvit/CuvitApplication.java
@@ -0,0 +1,13 @@
+package nl.bioinf.cuvit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuvitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuvitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/bioinf/cuvit/CuvitApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.bioinf.cuvit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuvitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
